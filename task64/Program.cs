93846c4cc85208271902fcbc3046e1858ccfc580
Ulyana <ashmishmish125@gmail.com>
2022-09-02 12:42:24 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение n. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от n до 1.
+// n = 5 -> "5, 4, 3, 2, 1"
+// n = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbersToN (int n)
+{
+if (n == 0) return;
+Console.Write(n + " ");
+PrintNumbersToN (n - 1);
+}
+
+PrintNumbersToN(n);
